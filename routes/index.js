f2d05d0066ebc71b8e83362cf01943f7839206aa
huangjoel <huangjoel@the-taj.ecs.vuw.ac.nz>
2016-03-11 10:48:46 +1300

    Have added in the basex code, successfully connects to the basex database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello' });
+var cheerio = require('cheerio');
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("open Colenso");
+//client.execute("xquery //movie[position() lt 10]",console.log);
+routes.get("/",function(req,res,next){
+client.execute("xquery for $movie in //movie[position() lt 10] " +
+"return <movie> {$movie/title} {$movie/id} </movie>",
+function (body) {
+var $ = cheerio.load(body);
+var list = [];
+$('movie').each(function(i,elem){
+var title = $(elem).find('title').text();
+var id = $(elem).find('id').text();
+var url = 'images/' + id + '.jpg';
+list.push({ p: title, image: url });
 });
-
-module.exports = router;
+res.render('index', { title: 'ECS Video Rental', images: list });	//render jade file with index.jade
+}
+);
+});
