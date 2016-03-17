00d1a0e04ed7cd96a14bd4f617caeab913b810c0
huangjoel <huangjoel@the-taj.ecs.vuw.ac.nz>
2016-03-17 15:58:20 +1300

    have listed all the files out

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,48 @@ client.execute("open Colenso");
 
 //client.execute("xquery //movie[position() lt 10]",console.log);
 router.get("/",function(req,res,next){
+/**
 client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; "+
 			"for $t in (//title) where $t= '24 August 1863: Hooker to Haast.' return db:path($t)",
 			//function(err,res) { if(!err) console.log(res.result)},
-	
+**/
+
+//client.execute("XQUERY doc('Colenso/McLean/private_letters/PrLMcL-0024.xml')",
+//client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; distinct-values(//name)",
+client.execute("XQUERY db:list('Colenso')",
+//function(err,res) { if(!err) console.log(res.result)}
+//client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+//"//name[@type = 'place' and position() = 1 and . = 'Manawarakau']",
 function (err,body) {
-	res.render('index', { title: 'ECS Video Rental', p: body.result});	//render jade file with index.jade
+	//var names= [];
+	var names = body.result.match(/[^\r\n]+/g);
+	//names.shift();
+	/**
+	var xmlFiles =[];
+	
+	for(var i=0;i<names.length;i++){
+		if(names[i].indexOf(".xml") > -1){
+			xmlFiles.push(names[i]);
+		}
+	} **/
+	
+	res.render('index', { title: 'ECS Video Rental', queryResult: names});	//render jade file with index.jade/
 }
 );
 });
+
+router.get("/:author/:ftype/:xml", function(req, res, next){
+		var author = req.params.author;
+		var ftype = req.params.ftype;			//for params if you want to change the page
+		var author = req.query.author;		//for search query, if you dont want to changes
+		
+		//need to check if this method works
+		//as this brackets above are not formatted correctly
+		console.log(author);
+		console.log(ftype);
+});
+
+
+
+
 module.exports = router;
